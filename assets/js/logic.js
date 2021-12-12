69175e6b1ff9de00aa6600a38f1059b05fdf2b1a
Tracy Tran <tracytran1189@gmail.com>
2021-12-12 01:27:49 -0600

    check answer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,120 +5,130 @@ var submitButton = document.getElementById('submit');
 var resultsContainer = document.getElementById('results');
 //add questions
 var quizQuestions = [{
-            question: "1. What does HTML stand for?",
-            answers: {
-                a: 'Hyper Text Markup Language',
-                b: 'Hot Mail',
-                c: 'How to Make Lasagna'
-            },
-
-            correctAnswer: 'a'
+        question: "1. What does HTML stand for?",
+        answers: {
+            a: 'Hyper Text Markup Language',
+            b: 'Hot Mail',
+            c: 'How to Make Lasagna'
         },
-        {
-            question: "2.What is the difference between an opening tag and a closing tag?",
-            answers: {
-                a: 'Opening tag has a / in front',
-                b: 'Closing tag has a / in front',
-                c: 'There is no difference'
-            },
-            correctAnswer: 'b'
+
+        correctAnswer: 'Hyper Text Markup Language'
+    },
+    {
+        question: "2.What is the difference between an opening tag and a closing tag?",
+        answers: {
+            a: 'Opening tag has a / in front',
+            b: 'Closing tag has a / in front',
+            c: 'There is no difference'
         },
-        {
-            question: "3.< br  / > What type of tag is this?",
-            answers: {
-                a: 'Break tag',
-                b: 'A broken one',
-                c: 'An opening tag'
-            },
-            correctAnswer: 'a'
+        correctAnswer: 'Closing tag has a / in front'
+    },
+    {
+        question: "3.< br  / > What type of tag is this?",
+        answers: {
+            a: 'Break tag',
+            b: 'A broken one',
+            c: 'An opening tag'
         },
-        {
-            question: "4.What does CSS stand for?",
-            answers: {
-                a: 'Computer Style Sheets',
-                b: 'Cascading Style Sheets',
-                c: 'Creative Style Sheets',
-                d: 'Control Style Sheets'
-            },
-            correctAnswer: 'b'
+        correctAnswer: 'Break tag'
+    },
+    {
+        question: "4.What does CSS stand for?",
+        answers: {
+            a: 'Computer Style Sheets',
+            b: 'Cascading Style Sheets',
+            c: 'Creative Style Sheets',
+            d: 'Control Style Sheets'
         },
-        {
-            question: "5.Which Of The Dialog Box Display a Message And a Data Entry Field?",
-            answers: {
-                a: 'Alert()',
-                b: 'Prompt()',
-                c: 'Confirm()',
-                d: 'Msg()'
-            },
-            correctAnswer: 'b'
+        correctAnswer: 'Cascading Style Sheets'
+    },
+    {
+        question: "5.Which Of The Dialog Box Display a Message And a Data Entry Field?",
+        answers: {
+            a: 'Alert()',
+            b: 'Prompt()',
+            c: 'Confirm()',
+            d: 'Msg()'
         },
-    ]
-    // var time = 100;
-    // var timerInterval;
-    // var gameIndex = -1;
+        correctAnswer: 'Prompt()'
+    },
+]
+var time = 100;
+var timerInterval;
+var gameIndex = -1;
+var numberOfQuestions = quizQuestions.length;
+var currentQuestionIndex = 0;
 
-// function startQuiz() {
-//     timerEl.textContent = time;
-//     timerInterval = setInterval(function() {
-//         time--;
-//         timerEl.textContent = time;
-//         if (time <= 0) {
-//             endQuiz();
-//         }
-//     }, 1000);
-
-//     gameIndex++;
-
-
-//     startBtn.onclick = startQuiz;
-//     showQuestion();
-// }
-
-// function endQuiz() {
-//     clearInterval(timerInterval);
-// }
-
-//create Quiz questions
-generateQuiz(quizQuestions, quizContainer, resultsContainer, submitButton);
-
-function generateQuiz(quizQuestions, quizContainer, resultsContainer, submitButton) {
-    function showQuestion(quizQuestions, quizContainer) {
-        var output = [];
-        var answers;
-        for (var i = 0; i < quizQuestions.length; i++) {
-
-            answers = [];
-
-            for (button in quizQuestions[i].answers) {
-                answers.push(
-                    '<button>' +
-                    // '<input type ="button" name="question' + i + '" value="' + letter + '">' +
-                    // letter + ':' +
-                    quizQuestions[i].answers[button] +
-                    '/button>'
-                );
-            }
-
-            output.push(
-
-                '<div class ="question">' + quizQuestions[i].question + '</div>' +
-                '<div class ="answers">' + answers.join('') + '</div>'
-
-            );
+function startQuiz() {
+    console.log('startQuiz');
+    timerEl.textContent = time;
+    timerInterval = setInterval(function() {
+        time--;
+        timerEl.textContent = time;
+        if (time <= 0) {
+            endQuiz();
         }
+    }, 1000);
 
-        quizContainer.innerHTML = output.join('');
-    }
+    gameIndex++;
+    generateQuiz();
+}
 
-    //get Results
-    function getResults() {}
-
-    showQuestion(quizQuestions, quizContainer);
-    //show results after click submit
-    submitButton.addEventListener('click', getResults);
+function endQuiz() {
+    clearInterval(timerInterval);
 }
 
 
+function clearQuestion() {
+    document.getElementById("question").remove();
+    document.getElementById("answers").remove();
+}
+
+function showQuestion() {
+
+    var question = document.createElement('div');
+    question.id = "question";
+    question.textContent = quizQuestions[currentQuestionIndex].question;
+    quizContainer.append(question);
+
+    var answersDiv = document.createElement('div');
+    answersDiv.id = "answers";
+
+    // var answers =[];
+    for (button in quizQuestions[currentQuestionIndex].answers) {
+        const answerButton = document.createElement('button');
+        answerButton.textContent = quizQuestions[currentQuestionIndex].answers[button];
+        answerButton.setAttribute('data-answer', quizQuestions[currentQuestionIndex].correctAnswer);
+        answerButton.onclick = checkAnswer;
+        // answers.push(answerButton);
+        answersDiv.append(answerButton);
+    }
+    quizContainer.append(answersDiv);
+}
+
+//check answers
+function checkAnswer(event) {
+    console.log('checkAnswer', event);
+    var correctAnswer = event.target.getAttribute("data-answer");
+    var selectedAnswer = event.target.textContent;
+    if (correctAnswer === selectedAnswer) {
+        console.log('correct');
+    } else {
+        console.log('false');
+    }
+
+    currentQuestionIndex++;
+    clearQuestion();
+    showQuestion();
+}
+
+function generateQuiz() {
+    console.log('generateQuiz')
+
+    showQuestion();
+    //show results after click submit
+    // submitButton.addEventListener('click', showResults);
+}
 
 
-// startBtn.onclick = startQuiz;
+startBtn.onclick = startQuiz;
