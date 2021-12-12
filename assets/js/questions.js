e19daab700aa12938b630e33af3a6e2c69c0175b
Tracy Tran <tracytran1189@gmail.com>
2021-12-11 18:24:44 -0600

    seprate questions js

---
+++ assets/js/questions.js
@@ -0,0 +1,49 @@
+var quizQuestions = [{
+        question: "1. What does HTML stand for?",
+        answers: {
+            a: 'Hyper Text Markup Language',
+            b: 'Hot Mail',
+            c: 'How to Make Lasagna'
+        },
+
+        correctAnswer: 'a'
+    },
+    {
+        question: "2.What is the difference between an opening tag and a closing tag?",
+        answers: {
+            a: 'Opening tag has a / in front',
+            b: 'Closing tag has a / in front',
+            c: 'There is no difference'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "3.< br  / > What type of tag is this?",
+        answers: {
+            a: 'Break tag',
+            b: 'A broken one',
+            c: 'An opening tag'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "4.What does CSS stand for?",
+        answers: {
+            a: 'Computer Style Sheets',
+            b: 'Cascading Style Sheets',
+            c: 'Creative Style Sheets',
+            d: 'Control Style Sheets'
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "5.Which Of The Dialog Box Display a Message And a Data Entry Field?",
+        answers: {
+            a: 'Alert()',
+            b: 'Prompt()',
+            c: 'Confirm()',
+            d: 'Msg()'
+        },
+        correctAnswer: 'b'
+    },
+]
